140037184eb0d8345124f6db24ea3b189e64d52c
cheorhyeon <r4560798@naver.com>
2025-05-21 19:44:58 +0900

    project setting

---
+++ src/test/java/hello/mybatispractice/MyBatisPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.mybatispractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBatisPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
